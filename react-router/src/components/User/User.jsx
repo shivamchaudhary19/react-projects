4da00834fc3dedf78203da5c4c9d50b970a4f4e5
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-07-14 21:24:39 +0530

    Implement React Router with nested routing and dynamic routes

---
+++ react-router/src/components/User/User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+  return (
+    <div className='bg-grey-600 text-white px-4 text-3xl'>User: {userid}</div>
+  )
+}
+
+export default User
