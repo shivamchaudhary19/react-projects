b14b4fb15b189f5bdd6518f473dad4b6eee8cad0
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-07-19 23:55:21 +0530

    fix: add useParams hook to extract userid in User component

--- react-router/src/components/User/User.jsx
+++ react-router/src/components/User/User.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function User() {
+  const { userid } = useParams()
+  
   return (
     <div className='bg-grey-600 text-white px-4 text-3xl'>User: {userid}</div>
   )
