c83614691da11439fded9eda90581c220d11d323
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-07-19 23:47:01 +0530

    feat: add responsive header component

---
+++ noisy-router/src/component/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react'
+
+export default function Home () {
+    return (
+      <div>Home</div>
+    )
+  }
---
+++ noisy-router/src/component/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class Product extends PureComponent {
+  render() {
+    return (
+      <div>Product</div>
+    )
+  }
+}
+
+export default Product
