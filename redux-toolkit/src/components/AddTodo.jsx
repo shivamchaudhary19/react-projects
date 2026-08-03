06a5c9386656adee02303b1760c4aa01bbf64cd8
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-03 23:27:24 +0530

    Add update functionality to Redux Toolkit Todo app

--- redux-toolkit/src/components/AddTodo.jsx
+++ redux-toolkit/src/components/AddTodo.jsx
@@ -1,20 +1,43 @@
 import React, {useState} from 'react'
-import {useDispatch} from 'react-redux'
-import {addTodo} from '../features/todo/todoSlice' 
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, updateTodo } from "../features/todo/todoSlice"; 
+import { useEffect } from "react";
 
 function AddTodo() {
 
     const [input, setInput] = useState('')
     const dispatch = useDispatch()
+    const { isEditing, editTodo } = useSelector(state => state);
 
-    const addTodoHandler = (e) => {
-        e.preventDefault()
-        dispatch(addTodo(input))
-        setInput('')
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if (!input.trim()) return;
+
+        if (isEditing) {
+
+            dispatch(updateTodo({
+                id: editTodo.id,
+                text: input
+            }));
+
+        } else {
+
+            dispatch(addTodo(input));
+
+        }
+
+        setInput("");
     }
 
+    useEffect(() => {
+        if (isEditing && editTodo) {
+            setInput(editTodo.text);
+        }
+    }, [editTodo, isEditing]);
+
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+    <form onSubmit={submitHandler} className="space-x-3 mt-12">
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -26,7 +49,7 @@ function AddTodo() {
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
-        Add Todo
+        {isEditing ? "Update Todo" : "Add Todo"}
       </button>
     </form>
   )
--- redux-toolkit/src/features/todo/todoSlice.js
+++ redux-toolkit/src/features/todo/todoSlice.js
@@ -1,10 +1,10 @@
 import {createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: 1, text: "Hello world"}]
+    todos: [{ id: 1, text: "Hello world" }],
+    editTodo: null,
+    isEditing: false,
 }
-
-
 
 export const todoSlice = createSlice({
     name: 'todo',
@@ -20,9 +20,33 @@ export const todoSlice = createSlice({
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload )
         },
+
+        startEditing: (state, action) => {
+            state.editTodo = action.payload;
+            state.isEditing = true;
+        },
+
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload;
+
+            const todo = state.todos.find((todo) => todo.id === id);
+
+            if (todo) {
+                todo.text = text;
+            }
+
+            state.editTodo = null;
+            state.isEditing = false;
+        },
+        
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {
+    addTodo,
+    removeTodo,
+    startEditing,
+    updateTodo
+} = todoSlice.actions;
 
 export default todoSlice.reducer
