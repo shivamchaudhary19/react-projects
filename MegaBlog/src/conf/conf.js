6ae954b289cc00ef038274d210f26393f852aa63
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-11 23:39:49 +0530

    Error fixed from .env and conf file

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,8 +1,8 @@
 const conf = {
     appwriteUrl: String(import.meta.env.
-        Vite_APPWRITE_URL),
+        VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.
-        VITE_APPWRITE_PROOJECT_ID),
+        VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.
         VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.
