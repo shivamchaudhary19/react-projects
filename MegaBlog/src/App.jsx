7a9d494f7b2623317862235665ba568520a46025
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-22 00:01:29 +0530

    add components of MegaBlog and react hook form

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
 
 function App() {
-  console.log(import.meta.env.Vite_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Hey , I'm Shivam Chaudhary</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TO-DO{/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./logo";
+
+export {
+    Header, 
+    Footer,
+    Container,
+    Logo
+}
